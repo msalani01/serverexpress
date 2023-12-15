a2be1b9d8b5d212f942e3155eb51788a7af5b1ca
unknown <msalani98@gmail.com>
2023-12-15 11:42:01 -0300

    algo imprime

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const ProductManager = require('./ProductManager'); 
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+const productManager = new ProductManager();
+
+
+
+app.get('/products.json', (req, res) => {
+  const allProducts = productManager.getAllProducts();
+  res.json(allProducts);
+});
+
+app.post('/products.json', (req, res) => {
+  const newProduct = req.body; 
+  productManager.addProduct(newProduct);
+  res.send('Producto añadido');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`escuchando en puerto ${port}`);
+});
