15293a181bae2a63d7f2434460356af206fbf15f
unknown <msalani98@gmail.com>
2023-12-16 12:34:49 -0300

    funcionamiento

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const ProductManager = require('./ProductManager'); 
+
 const app = express();
 const port = 3000;
 
-
-
 const productManager = new ProductManager();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Hola');
+});
 
-app.get('/products.json', (req, res) => {
+app.get('/products', (req, res) => {
   const allProducts = productManager.getAllProducts();
   res.json(allProducts);
 });
 
-app.post('/products.json', (req, res) => {
-  const newProduct = req.body; 
+app.post('/products', (req, res) => {
+  const newProduct = req.body;
   productManager.addProduct(newProduct);
-  res.send('Producto añadido');
+  res.send('producto añadido');
 });
 
 app.listen(port, () => {
-  console.log(`escuchando en puerto ${port}`);
+  console.log(`escuchando puerto ${port}`);
 });
