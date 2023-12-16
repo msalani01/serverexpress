a3b1f0c4ab1d48e8f457ca7f927bef02a0f8ae7b
unknown <msalani98@gmail.com>
2023-12-16 17:49:22 -0300

    entrega inc

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs'); // Agregamos el módulo 'fs' para manejar archivos
-const path = require('path'); // Agregamos el módulo 'path' para manejar rutas
-const ProductManager = require('./ProductManager'); // Ajusta la ruta según la ubicación real del archivo
+const fs = require('fs'); 
+const path = require('path'); 
+const ProductManager = require('./ProductManager'); 
 
 const app = express();
 const port = 3000;
 
 const productManager = new ProductManager();
 
-// Cargar productos desde el archivo products.json al iniciar la aplicación
-const productsFilePath = path.join(__dirname, 'products.json');
+const productsFilePath = path.join(__dirname, 'src', 'products.json');
 
 try {
   const productsData = fs.readFileSync(productsFilePath, 'utf8');
@@ -22,13 +21,23 @@ try {
   console.error('Error al cargar productos desde el archivo:', error.message);
 }
 
-// Ruta para obtener todos los productos
+
+app.get('/products/:pid', (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const product = productManager.getProductById(productId);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+  }
+});
+
+
 app.get('/products', (req, res) => {
   const { limit } = req.query;
-
   let allProducts = productManager.getAllProducts();
 
-  // Si se proporciona un límite, ajusta la lista de productos
   if (limit) {
     const limitNumber = parseInt(limit);
     allProducts = allProducts.slice(0, limitNumber);
@@ -40,9 +49,9 @@ app.get('/products', (req, res) => {
 app.post('/products', (req, res) => {
   const newProduct = req.body;
   productManager.addProduct(newProduct);
-  res.send('Product added successfully');
+  res.send('producto agregado');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app escuchando en ${port}`);
 });
