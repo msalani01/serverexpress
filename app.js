5c03aff93714878d05125eb3f3595ed0264f477a
unknown <msalani98@gmail.com>
2023-12-16 15:29:15 -0300

    path

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ProductManager = require('./ProductManager'); 
+const fs = require('fs'); // Agregamos el módulo 'fs' para manejar archivos
+const path = require('path'); // Agregamos el módulo 'path' para manejar rutas
+const ProductManager = require('./ProductManager'); // Ajusta la ruta según la ubicación real del archivo
 
 const app = express();
 const port = 3000;
 
 const productManager = new ProductManager();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Cargar productos desde el archivo products.json al iniciar la aplicación
+const productsFilePath = path.join(__dirname, 'products.json');
 
+try {
+  const productsData = fs.readFileSync(productsFilePath, 'utf8');
+  const products = JSON.parse(productsData);
+  products.forEach((product) => {
+    productManager.addProduct(product);
+  });
+} catch (error) {
+  console.error('Error al cargar productos desde el archivo:', error.message);
+}
 
+// Ruta para obtener todos los productos
 app.get('/products', (req, res) => {
   const { limit } = req.query;
 
   let allProducts = productManager.getAllProducts();
 
-  
+  // Si se proporciona un límite, ajusta la lista de productos
   if (limit) {
     const limitNumber = parseInt(limit);
     allProducts = allProducts.slice(0, limitNumber);
@@ -28,9 +40,9 @@ app.get('/products', (req, res) => {
 app.post('/products', (req, res) => {
   const newProduct = req.body;
   productManager.addProduct(newProduct);
-  res.send('producto agregado');
+  res.send('Product added successfully');
 });
 
 app.listen(port, () => {
-  console.log(`escuchando en puerto ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
