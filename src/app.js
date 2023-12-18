24b1e0799a87402faed481d421a339da9ede77ad
unknown <msalani98@gmail.com>
2023-12-18 16:57:53 -0300

    FUNCIONAgit add .git add .git add .git add .git add .(algo)

--- src/app.js
+++ src/app.js
@@ -7,28 +7,25 @@ const { send } = require('process');
 const app = express();
 const port = 8080;
 
-const productManager = new ProductManager();
+const productManager = new ProductManager('C:/Users/M/Documents/Dev/Servidor_Express/src/products.json');
 var __dirname = "C:/Users/M/Documents/Dev/Servidor_Express"
 
 const productsFilePath = path.join(__dirname, 'src', 'products.json');
 
 async function cargarProductos() {
-
-try {
-  const productsData = await fs.readFile(productsFilePath, 'utf8');
-  const products = JSON.parse(productsData);
-  products.forEach((product) => {
-    productManager.addProduct(product);
-  });
-} catch (error) {
-  console.error('Error al cargar productos desde el archivo:', error.message);
+  try {
+    console.log(productsFilePath);  
+    const productsData = await fs.readFile(productsFilePath, 'utf8');
+    const products = JSON.parse(productsData);
+    products.forEach((product) => {
+      productManager.addProduct(product);
+    });
+  } catch (error) {
+    console.error('Error al cargar productos desde el archivo:', error.message);
+  }
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-}
 
 app.get('/products/:pid', (req, res) => {
   const productId = parseInt(req.params.pid);
