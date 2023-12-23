08252c5c667fc83f704c5752983ee7241a58d121
unknown <msalani98@gmail.com>
2023-12-23 19:16:00 -0300

    entrega

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
 const express = require('express');
-const fs = require('fs').promises; 
-const path = require('path'); 
-const ProductManager = require('./ProductManager'); 
+const fs = require('fs').promises;
+const path = require('path');
+const ProductManager = require('./ProductManager');
 const session = require('express-session');
+
 const app = express();
 const port = 8080;
-const router = express.Router(); 
-
+const router = express.Router();
 
 app.use('/api/carts', router);
 
 const productManager = new ProductManager('C:/Users/M/Documents/Dev/Servidor_Express/src/products.json');
 
-
 const productsFilePath = path.join(__dirname, 'src', 'products.json');
 
 app.use(session({
-  secret: 'tu_secreto', 
+  secret: 'tu_secreto',
   resave: false,
   saveUninitialized: true
 }));
 
 async function cargarProductos() {
   try {
-    console.log(productsFilePath);  
     const productsData = await fs.readFile(productsFilePath, 'utf8');
     const products = JSON.parse(productsData);
     products.forEach((product) => {
@@ -37,16 +35,16 @@ async function cargarProductos() {
 const carts = [];
 
 router.post('/', (req, res) => {
-  // Generar un ID único para el nuevo carrito
+
   const cartId = generateCartId();
 
-  // Crear un nuevo carrito con la estructura requerida
+
   const newCart = {
     id: cartId,
     products: [],
   };
 
-  // Agregar el nuevo carrito al array de carritos
+
   carts.push(newCart);
 
   res.status(201).json(newCart);
@@ -56,23 +54,34 @@ function generateCartId() {
   return Math.random().toString(36).substr(2, 9);
 }
 
-
-module.exports = router;
-
-
-// Ruta para agregar un producto al carrito
-router.post('/:cartId/products/', (req, res) => {
+router.get('/:cartId', (req, res) => {
   const cartId = req.params.cartId;
-  const productId = req.body.productId; // Suponiendo que se proporciona el ID del producto
 
-  // Encontrar el carrito correspondiente por ID
+
   const cart = carts.find(cart => cart.id === cartId);
 
   if (!cart) {
     return res.status(404).json({ error: 'Carrito no encontrado' });
   }
 
-  // Agregar el producto al carrito (puedes ajustar según tu lógica de productos)
+
+  const productsInCart = cart.products;
+
+  res.status(200).json(productsInCart);
+});
+
+router.post('/:cartId/products/', (req, res) => {
+  const cartId = req.params.cartId;
+  const productId = req.body.productId; 
+
+
+  const cart = carts.find(cart => cart.id === cartId);
+
+  if (!cart) {
+    return res.status(404).json({ error: 'Carrito no encontrado' });
+  }
+
+
   const productToAdd = productManager.getProductById(productId);
 
   if (!productToAdd) {
@@ -84,8 +93,30 @@ router.post('/:cartId/products/', (req, res) => {
   res.status(201).json(cart);
 });
 
-module.exports = router;
+router.post('/:cid/product/:pid', (req, res) => {
+  const cartId = req.params.cid;
+  const productId = req.params.pid;
 
+
+  const cart = carts.find((cart) => cart.id === cartId);
+
+  if (!cart) {
+    return res.status(404).json({ error: 'Carrito no encontrado' });
+  }
+
+
+  const existingProduct = cart.products.find((product) => product.product === productId);
+
+  if (existingProduct) {
+
+    existingProduct.quantity++;
+  } else {
+
+    cart.products.push({ product: productId, quantity: 1 });
+  }
+
+  res.status(201).json(cart);
+});
 
 app.get('/products/:pid', (req, res) => {
   const productId = parseInt(req.params.pid);
@@ -102,13 +133,11 @@ app.put('/products/:pid', (req, res) => {
   const productId = parseInt(req.params.pid);
   const { title, description, code, price, stock, category, thumbnails } = req.body;
 
-
   const existingProduct = productManager.getProductById(productId);
 
   if (!existingProduct) {
     return res.status(404).json({ error: 'Producto no encontrado' });
   }
-
 
   existingProduct.title = title || existingProduct.title;
   existingProduct.description = description || existingProduct.description;
@@ -118,9 +147,7 @@ app.put('/products/:pid', (req, res) => {
   existingProduct.category = category || existingProduct.category;
   existingProduct.thumbnails = thumbnails || existingProduct.thumbnails;
 
-
   productManager.saveProducts();
-
 
   res.status(200).json(existingProduct);
 });
@@ -128,22 +155,18 @@ app.put('/products/:pid', (req, res) => {
 app.delete('/products/:pid', (req, res) => {
   const productId = parseInt(req.params.pid);
 
-  
   const productIndex = productManager.getAllProducts().findIndex(product => product.id === productId);
 
   if (productIndex === -1) {
     return res.status(404).json({ error: 'Producto no encontrado' });
   }
 
-
   productManager.getAllProducts().splice(productIndex, 1);
-
 
   productManager.saveProducts();
 
   res.status(204).send();
 });
-
 
 app.get('/products', (req, res) => {
   const { limit } = req.query;
@@ -160,15 +183,12 @@ app.get('/products', (req, res) => {
 app.post('/products', (req, res) => {
   const newProduct = req.body;
   productManager.addProduct(newProduct);
-  
 
   const { title, description, code, price, stock, category, thumbnails } = req.body;
 
-  
   if (!title || !description || !code || !price || !stock || !category) {
     return res.status(400).json({ error: 'Todos los campos son obligatorios excepto thumbnails' });
   }
-
 
   const newProductcart = {
     id: productManager.getNextId(),
@@ -176,23 +196,18 @@ app.post('/products', (req, res) => {
     description,
     code,
     price,
-    status: true, 
+    status: true,
     stock,
     category,
-    thumbnails: thumbnails || [], 
+    thumbnails: thumbnails || [],
   };
-
 
   productManager.addProduct(newProductcart);
 
-
   res.status(201).json(newProductcart);
-  res.send('producto agregado');
 });
 
-app.listen(port, () => {
-  console.log(`app escuchando en ${port}`);
-});
+
 
 app.get('/cart', (req, res) => {
   const productsInCart = obtenerProductosEnElCarrito(req);
@@ -200,19 +215,22 @@ app.get('/cart', (req, res) => {
 });
 
 
+
+
 function obtenerProductosEnElCarrito(req) {
- 
   const session = req.session;
 
-
   if (session.cart) {
- 
     return session.cart;
   } else {
-   
     return [];
   }
 }
 
+app.listen(port, () => {
+  console.log(`app escuchando en ${port}`);
+});
 
-cargarProductos()
+cargarProductos();
+
+module.exports = router;
